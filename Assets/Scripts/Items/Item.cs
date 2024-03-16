92e46ed22a2ef004d549f104b70d4131066436f3
Intronirisme <38733938+Intronirisme@users.noreply.github.com>
2024-03-16 17:54:27 +0100

    Update Item.cs

--- Assets/Scripts/Items/Item.cs
+++ Assets/Scripts/Items/Item.cs
@@ -11,24 +11,44 @@ public enum ProjectileTypes
     Burn
 }
 
+[RequireComponent(typeof(Rigidbody))]
 public class Item : MonoBehaviour
 {
     [Header("Interactions")]
-    public bool PlayerCanUse = false;
-    public bool PlayerCanThrow = false;
     public ProjectileTypes ProjectileType = ProjectileTypes.Null;
+    public float SnapDuration = .6f;
 
+    protected bool PlayerCanUse = false;
     public bool CanUse { get { return PlayerCanUse; } }
+    protected bool PlayerCanThrow = false;
     public bool CanThrow { get { return PlayerCanThrow; } }
+
+    private bool _isHeld = false;
+
+    private float _remainingSnap;
+
+    void Update()
+    {
+        if (_isHeld && _remainingSnap > 0)
+        {
+            _remainingSnap -= Time.deltaTime;
+            transform.position = Vector3.Lerp(transform.parent.position, transform.position, _remainingSnap/SnapDuration);
+            transform.rotation = Quaternion.Slerp(transform.parent.rotation, transform.rotation, _remainingSnap/SnapDuration);
+        }
+    }
 
     public void Pickup(GameObject AttachPoint)
     {
-
+        transform.parent = AttachPoint.transform;
+        _isHeld = true;
+        _remainingSnap = SnapDuration;
     }
 
     public void Drop()
     {
-
+        _isHeld = false;
+        transform.parent = null;
+        _remainingSnap = 0;
     }
 
     public void Use()
