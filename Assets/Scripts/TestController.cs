16dd0daa4013fd72d25d8745fb5a2af64d723207
Boushi33 <A.JOURDANNAUD@lahorde.school>
2024-03-16 11:31:10 +0100

    Added a player script

--- Assets/Scripts/TestController.cs
+++ Assets/Scripts/TestController.cs
@@ -1,5 +1,3 @@
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class TestController : MonoBehaviour
@@ -8,11 +6,6 @@ public class TestController : MonoBehaviour
     Vector2 rotation = Vector2.zero;
     const string xAxis = "Mouse X"; 
     const string yAxis = "Mouse Y";
-
-    private float _range = 10f;
-
-    private IInteractable _itemInHand = null;
-    private List<IInteractable> _itemsInLayer = new List<IInteractable>(); //add a list for each timer layer ?
 
     // Update is called once per frame
     void Update()
@@ -29,76 +22,5 @@ public class TestController : MonoBehaviour
         var yQuat = Quaternion.AngleAxis(rotation.y, Vector3.left);
 
         transform.localRotation = xQuat * yQuat;
-
-        if (Input.GetKeyDown(KeyCode.E)) 
-        {
-            TakeItem();
-        }
-        if (Input.GetMouseButtonDown(0))
-        {
-            UseItem();
-            PlayItem();
-        }
-        if (Input.GetMouseButtonDown(1))
-        {
-            ThrowItem();
-        }
-    }
-
-    private void TakeItem()
-    {
-        RaycastHit hit;
-
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, _range))
-        {
-            if (hit.collider != null)
-            {
-                if (hit.collider.TryGetComponent(out IInteractable intertactable))
-                {
-                    intertactable.Take();
-                    _itemInHand = intertactable;
-
-                    //watch which time layer the hand is so we know in which list we put the item
-                    _itemsInLayer.Add(_itemInHand);
-                }
-            }
-        }
-    }
-
-    private void UseItem()
-    {
-        if (_itemInHand != null)
-        {
-            _itemInHand.Use();
-        }
-        else
-        {
-            Debug.Log("No item in hand");
-        }
-    }
-
-    private void ThrowItem()
-    {
-        if (_itemInHand != null)
-        {
-            _itemInHand.Throw();
-        }
-        else
-        {
-            Debug.Log("No item in hand to be thrown");
-        }
-    }
-
-    private void PlayItem()
-    {
-        if (_itemsInLayer.Count > 0)
-        {
-            foreach (var item in _itemsInLayer)
-            {
-                item.Play();
-            }
-
-            _itemsInLayer.Clear();
-        }
     }
 }
